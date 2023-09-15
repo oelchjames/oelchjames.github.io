f4386cbc4e99f4c0dc944c8aca37834c9ea29385
oelchjames <119647524+oelchjames@users.noreply.github.com>
2023-09-15 23:57:18 +0800

    Delete IT Capstone 2 directory

--- IT Capstone 2/Script/script.js
+++
@@ -1,8 +0,0 @@
-let dropdown = document.querySelectorAll(".dropdown");
-    for(var i = 0; i < dropdown.length; i++){
-        dropdown[i].addEventListener("click", (e)=>{
-        let dropdownParent = e.target.parentElement.parentElement;
-        console.log(dropdownParent);
-        dropdownParent.classList.toggle("showMenu");
-        });
-    }
